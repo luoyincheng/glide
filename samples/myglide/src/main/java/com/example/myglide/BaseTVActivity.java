99c59c0361fc5ebb162713fec101755ae8098934
luoyincheng <yincheng.luo@gmail.com>
2021-01-25 18:16:20 +0800

    adf

--- samples/myglide/src/main/java/com/example/myglide/BaseTVActivity.java
+++ samples/myglide/src/main/java/com/example/myglide/BaseTVActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
@@ -13,7 +14,6 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
-import androidx.appcompat.widget.AppCompatTextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintLayout.LayoutParams;
 import com.bumptech.glide.Glide;
@@ -26,21 +26,27 @@ public abstract class BaseTVActivity extends AppCompatActivity {
    private ConstraintLayout mRootContainer;
 
    List<String> mUrls = Arrays.asList(
-         "https://clubimg.club.vmall.com/data/attachment/forum/201905/17/082652qm7mayijp9eua6d5.jpg",
          "https://ottimg.cdn.pandora.xiaomi.com/e52cdffbf10abaef0a5daeebeebb48cc.jpg.webp",
-         "https://img.leitu123.com/article/UploadPic/2012-7/20127271358077420.jpg",
          "https://ottimg.cdn.pandora.xiaomi.com/6eacc8d2da4296ceea1c54ef38da4f83.jpg.webp",
-         "https://images.hdqwalls.com/download/small-memory-lp-1920x1080.jpg",
          "https://ottimg.cdn.pandora.xiaomi.com/a1f6deee50943e8c6e64c21ca2851b97.png.webp",
-         "https://images.hdqwalls.com/download/not-in-mood-4k-qy-1920x1080.jpg",
          "https://ottimg.cdn.pandora.xiaomi.com/d22c2450041578e96ac3aa258998122f.jpg.webp",
-         "https://www.winwallpapers.net/w1/2018/08/Windows-Logo-Surface-Cubes-1920x1080.jpg",
          "https://image.cdn.pandora.xiaomi.com/mitv/10007/0/b7eece58ee3aa3ab572bf7bec51f5ad2.webp",
-         "https://www.winwallpapers.net/w1/2018/08/Windows-Clouds-Sky-Blue-White-1920x1080.jpg",
          "https://ottimg.cdn.pandora.xiaomi.com/6b3b60ce22ffe8e9501ae9759433367c.jpeg.webp",
-         "https://www.winwallpapers.net/w1/2018/08/Windows-Logo-Operating-System-1920x1080.jpg",
-         "https://ottimg.cdn.pandora.xiaomi.com/ff5eef11775003a9a14c537bbc8f2f08.webp"
-   );
+         "https://ottimg.cdn.pandora.xiaomi.com/ff5eef11775003a9a14c537bbc8f2f08.webp",
+         "https://image.cdn.pandora.xiaomi.com/mitv/10007/0/24999f2fe9613479866b071dde61fd2a.webp",
+         "https://ottimg.cdn.pandora.xiaomi.com/48da4e170a22c830dc7c18ddc21188df.jpg.webp",
+         "https://image.cdn.pandora.xiaomi.com/mitv/10007/0/9bc9afecbbd2bfb33716d25e9c7b95a9.webp",
+         "https://image.cdn.pandora.xiaomi.com/mitv/10007/0/3040eb9f7d3cae1ae0dc819c4b658340.webp",
+         "https://ottimg.cdn.pandora.xiaomi.com/6a3c88bb67f22d193d51e83a87f0d0d2.jpg.webp",
+         "https://ottimg.cdn.pandora.xiaomi.com/e5fa6ccb25babf9c66b353b8c7717df9.webp",
+         "https://image.cdn.pandora.xiaomi.com/mitv/10007/0/adfcbb7d521122f9f99478c963be8083.webp",
+         "https://ottimg.cdn.pandora.xiaomi.com/a77b7c25ef5ce1cb74e30396da9efae7.png.webp",
+         "https://image.cdn.pandora.xiaomi.com/mitv/10007/0/9c90d12633ce44483a1252fe72771f6b.webp",
+         "https://ottimg.cdn.pandora.xiaomi.com/790d7adf9880293ab5d67dd69f761d2f.jpg.webp",
+         "https://ottimg.cdn.pandora.xiaomi.com/9468936113b2abef9b224ccf67f37088.webp",
+         "https://ottimg.cdn.pandora.xiaomi.com/04fc547a6908fa5cd80d26f03d6c2efc.webp",
+         "https://image.cdn.pandora.xiaomi.com/mitv/10007/0/b7eece58ee3aa3ab572bf7bec51f5ad2.webp"//重复
+         );
 
    final OnFocusChangeListener onFocusChangeListener = new OnFocusChangeListener() {
       @Override
@@ -59,10 +65,10 @@ public abstract class BaseTVActivity extends AppCompatActivity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_base_tv);
       mRootContainer = findViewById(R.id.csl_tv_base);
+      LayoutInflater layoutInflater = LayoutInflater.from(this);
+      ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT);
+      mRootContainer.addView(layoutInflater.inflate(withLayoutId(), null, false), layoutParams);
       fullFillWithImageView();
-//      LayoutInflater layoutInflater = LayoutInflater.from(this);
-//      ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.MATCH_PARENT);
-//      mRootContainer.addView(layoutInflater.inflate(withLayoutId(), null, false), layoutParams);
       registerFocusListenerForAllViews(getWindow().getDecorView());
    }
 
@@ -88,14 +94,11 @@ public abstract class BaseTVActivity extends AppCompatActivity {
       GridLayout gridLayout = new GridLayout(this);
       gridLayout.setColumnCount(4);
       gridLayout.setRowCount(5);
-      gridLayout.setBackground(new ColorDrawable(Color.parseColor("#ff0000")));
-//      gridLayout.setPadding(100, 100, 100, 100);
       for (int i = 0; i < gridLayout.getRowCount(); i++) {
          for (int j = 0; j < gridLayout.getColumnCount(); j++) {
             PrettyLogger.commonLog("i:" + i, "j:" + j);
-//            AppCompatImageView imageView = new AppCompatImageView(this);
-            AppCompatTextView textView= new AppCompatTextView(this);
-            textView.setBackground(new ColorDrawable(Color.parseColor("#3c5f78")));
+            AppCompatImageView imageView = new AppCompatImageView(this);
+            imageView.setBackground(new ColorDrawable(Color.parseColor("#ff0000")));
             GridLayout.Spec rowSpec = GridLayout.spec(i, 1.0f);
             GridLayout.Spec columnSpec = GridLayout.spec(j, 1.0f);
             GridLayout.LayoutParams gridLayoutParams = new GridLayout.LayoutParams(rowSpec, columnSpec);
@@ -103,9 +106,10 @@ public abstract class BaseTVActivity extends AppCompatActivity {
             gridLayoutParams.width = 320;
             gridLayoutParams.height = 180;
             gridLayoutParams.setMargins(5, 5, 5, 5);
-            gridLayout.addView(textView, gridLayoutParams);
-//            PrettyLogger.commonLog("imageView:" + gridLayout.getMeasuredWidth() + ":" + gridLayout.getHeight() + ":" + imageView.getWidth() + ':' + imageView.getHeight());
-//            Glide.with(this).load(i * gridLayout.getColumnCount() + j).into(imageView);
+            gridLayout.addView(imageView, gridLayoutParams);
+            int curIndex = i * gridLayout.getColumnCount() + j;
+            if (curIndex > mUrls.size() - 1) { break; }
+            Glide.with(this).load(mUrls.get(curIndex)).into(imageView);
          }
       }
       mRootContainer.addView(gridLayout, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
